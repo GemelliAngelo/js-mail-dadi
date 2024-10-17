63e18c18e1577ebf102fb58eaea60cef29a290c9
GemelliAngelo <gemeangelo27@gmail.com>
2024-10-17 14:57:26 +0200

    complete dadi

--- JS/main.js
+++ JS/main.js
@@ -17,8 +17,21 @@ for (let i = 0; i < validEmails.length; i++) {
     isUserEmailValid = true;
   }
 }
-console.log(isUserEmailValid);
+console.log(isUserEmailValid ? "BENVENUTO" : "CI DISPIACE. NON PUOI ACCEDERE");
 
 // ! Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+const userNum = Math.floor(Math.random() * 6) + 1;
+const comNum = Math.floor(Math.random() * 6) + 1;
+console.log(`Il tuo numero: ${userNum}`);
+console.log(`Il numero del computer: ${comNum}`);
+
+if (userNum > comNum) {
+  console.log("COMPLIMENTI!! HAI VINTO");
+} else if (userNum < comNum) {
+  console.log("HAI PERSO!! RITENTA");
+} else {
+  console.log("PARITÀ!! RITENTA");
+}
