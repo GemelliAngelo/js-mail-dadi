ebab6fbf4e9da45d75febe79fd045961c8ccd39a
GemelliAngelo <gemeangelo27@gmail.com>
2024-10-17 15:30:55 +0200

    add dadi decoration

--- JS/dadi.js
+++ JS/dadi.js
@@ -4,13 +4,20 @@
 
 const userNum = Math.floor(Math.random() * 6) + 1;
 const comNum = Math.floor(Math.random() * 6) + 1;
-console.log(`Il tuo numero: ${userNum}`);
-console.log(`Il numero del computer: ${comNum}`);
+document.getElementById(
+  "game"
+).innerHTML = `<h2 class="text-center py-3">Il tuo numero: ${userNum}</h2> <br/> <h2 class="text-center py-3">Il numero del computer: ${comNum}</h2>`;
 
 if (userNum > comNum) {
-  console.log("COMPLIMENTI!! HAI VINTO");
+  document.getElementById(
+    "results"
+  ).innerHTML = `<h2 class="text-center py-3 text-success">HAI VINTO!!!</h2>`;
 } else if (userNum < comNum) {
-  console.log("HAI PERSO!! RITENTA");
+  document.getElementById(
+    "results"
+  ).innerHTML = `<h2 class="text-center py-3 text-danger">HAI PERSO!!! RITENTA</h2>`;
 } else {
-  console.log("PARITÀ!! RITENTA");
+  document.getElementById(
+    "results"
+  ).innerHTML = `<h2 class="text-center py-3 text-warning">PARITÀ!!! RITENTA</h2>`;
 }
