d981959be988c074a2be6fee13a4da7bede90cfc
GemelliAngelo <gemeangelo27@gmail.com>
2024-10-17 14:14:40 +0200

    add js

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,23 @@
-// Mail
+// ! Mail
 // Chiedi all'utente la sua email,
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull'esito del controllo.
+const validEmails = [
+  "hello@gmail.com",
+  "me@gmail.com",
+  "here@gmail.com",
+  "admin@gmail.com",
+];
+const userEmail = prompt("Inserisci la tua Email");
 
-// Gioco dei dadi
+for (let i = 0; i < validEmails.length; i++) {
+  if (validEmails[i] === userEmail) {
+    console.log("Benvenuto");
+  } else {
+    console.log("Non puoi accedere");
+  }
+}
+
+// ! Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
